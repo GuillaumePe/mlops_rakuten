983b53bf7a8702e3c1c8ce0c89e88b588e431503
GuillaumePe <guillaumepedron@hotmail.com>
2025-04-20 07:39:24 +0000

    commit de la création des chunked_text_files de 0000 à 0005

--- src/features/build_text_features.py
+++ src/features/build_text_features.py
@@ -1,33 +1,43 @@
 import polars as pl
-import numpy as np
 from transformers import DistilBertTokenizerFast, DistilBertModel
 import gc
 from utils import extract_features_in_batches, clean_description, log_progress
 import time
 import os
-
+from dvclive import Live
+import subprocess
 # Paramètres
 INPUT_CSV = "/home/ubuntu/mar25_cmlops_rakuten/data/raw_data/X_train.csv"
 LIST_TEXT_COLUMN = ["designation","description"]
-LIST_ID_COLUMNs = ["imageid","productid"]
-OUTPUT_DIR = "/home/ubuntu/mar25_cmlops_rakuten/data/preprocessed/chunked_text_files "
+LIST_ID_COLUMNS = ["imageid","productid"]
+OUTPUT_DIR = "/home/ubuntu/mar25_cmlops_rakuten/data/preprocessed/chunked_text_files"
 
 # Chargement des données et nettoyages
 data = pl.read_csv(INPUT_CSV)
 data = data.with_columns(pl.concat_str([pl.col(LIST_TEXT_COLUMN[0]),pl.col(LIST_TEXT_COLUMN[1])],separator=". ",ignore_nulls=True).alias('_'.join(LIST_TEXT_COLUMN)))
 data = data.with_columns(pl.col('_'.join(LIST_TEXT_COLUMN)).map_elements(lambda text:clean_description(text), return_dtype=pl.Utf8)).drop(LIST_TEXT_COLUMN)
-data = data.sort(LIST_ID_COLUMNs)
+data = data.sort(LIST_ID_COLUMNS)
 
 tokenizer = DistilBertTokenizerFast.from_pretrained("distilbert-base-uncased")
 model = DistilBertModel.from_pretrained("distilbert-base-uncased")
 
-start = time.time()
 batch_size = 10000
+###
+
+total_batches = (data.height + batch_size - 1) // batch_size
+start_time = time.time()
 
 for i in range(0, data.height, batch_size):
+    batch_start = time.time()
+    batch_num = i // batch_size
+    output_file = os.path.join(OUTPUT_DIR, f"features_text_chunk_{batch_num:04d}.parquet")
+    
+    if os.path.exists(output_file):
+        continue
+    print(f"[Batch {batch_num}/{total_batches}] Processing...")
     sub_data = data.slice(i, batch_size)
     # extrait les ids
-    ids_df = sub_data.select(LIST_ID_COLUMNs)
+    ids_df = sub_data.select(LIST_ID_COLUMNS)
     # extrait les textes
     texts = sub_data.select('_'.join(LIST_TEXT_COLUMN)).to_series().to_list()
 
@@ -40,13 +50,13 @@ for i in range(0, data.height, batch_size):
 
     # Ajoute les colonnes productid et imageid 
     final_sub_df = pl.concat([ids_df, features_pl],how="horizontal")
-    output_file = os.path.join(OUTPUT_DIR, f"features_text_chunk_{i//batch_size:04d}.parquet")
-    final_sub_df.write_parquet(output_file) 
+    
+    if not os.path.exists(OUTPUT_DIR):
+        print(f"Dossier {OUTPUT_DIR} supprimé pendant l'exécution ! Je le recrée.")
+        os.makedirs(OUTPUT_DIR, exist_ok=True)
+    
+    final_sub_df.write_parquet(output_file)
     del sub_data, texts, embeddings, features_pl, ids_df, final_sub_df 
     gc.collect()
-    log_progress(
-            current=(i + batch_size) // batch_size,
-            total=(len(texts) + batch_size - 1) // batch_size,
-            start_time=start
-        )
-
+    log_progress(batch_num + 1, total_batches, start_time)
+   
