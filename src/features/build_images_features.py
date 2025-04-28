9809bd7c498693b1750b438fcc7fac986b99b1ed
GuillaumePe <guillaumepedron@hotmail.com>
2025-04-28 15:34:03 +0000

    mise à jour des scripts notebooks/exploration.ipynb src/features/build_images_features.py src/features/build_text_features.py en vu de la création du predict

--- src/features/build_images_features.py
+++ src/features/build_images_features.py
@@ -13,59 +13,66 @@ LIST_ID_COLUMNS = ["imageid", "productid"]
 IMAGE_FOLDER = "/home/ubuntu/mar25_cmlops_rakuten/data/raw_data/images/image_train"
 OUTPUT_DIR = "/home/ubuntu/mar25_cmlops_rakuten/data/preprocessed/chunked_image_files"
 
-# Chargement des données et nettoyages
-data_text = pl.read_csv(INPUT_CSV)
-data_text = data_text.select(LIST_ID_COLUMNS).sort(LIST_ID_COLUMNS)
+def build_images_features_func(INPUT_CSV,LIST_ID_COLUMNS,IMAGE_FOLDER,OUTPUT_DIR):
+   
+    #chargement de ResNet18 pré-entrainé
+    model = resnet18(weights=ResNet18_Weights.DEFAULT)  
+    model = torch.nn.Sequential(*list(model.children())[:-1])  # Retirer la couche Fully Connected
+    model.eval()
+     # Chargement des données et nettoyages
+    data_text = pl.read_csv(INPUT_CSV)
+    data_text = data_text.select(LIST_ID_COLUMNS).sort(LIST_ID_COLUMNS)
 
-data_image = data_text.with_columns(("image_"+pl.col(LIST_ID_COLUMNS[0]).cast(pl.Utf8)+"_product_"+pl.col(LIST_ID_COLUMNS[1]).cast(pl.Utf8)+".jpg").alias("image_path"))
-
-#chargement de ResNet18 pré-entrainé
-model = resnet18(weights=ResNet18_Weights.DEFAULT)  
-model = torch.nn.Sequential(*list(model.children())[:-1])  # Retirer la couche Fully Connected
-model.eval()
+    data_image = data_text.with_columns(("image_"+pl.col(LIST_ID_COLUMNS[0]).cast(pl.Utf8)+"_product_"+pl.col(LIST_ID_COLUMNS[1]).cast(pl.Utf8)+".jpg").alias("image_path"))
 
 
-#preprocess des images
-preprocess = transforms.Compose([
-    transforms.Resize(256),
-    transforms.CenterCrop(224),
-    transforms.ToTensor(),
-    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
-])
 
-batch_size = 10000
-total_batches = (data_image.height + batch_size - 1) // batch_size
-start_time = time.time()
 
-for i in range(0, data_image.height, batch_size):
-    batch_start = time.time()
-    batch_num = i // batch_size
-    output_file = os.path.join(OUTPUT_DIR, f"features_image_chunk_{batch_num:04d}.parquet")
+    #preprocess des images
+    preprocess = transforms.Compose([
+        transforms.Resize(256),
+        transforms.CenterCrop(224),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+    ])
 
-    if os.path.exists(output_file):
-        continue
+    batch_size = 10000
+    total_batches = (data_image.height + batch_size - 1) // batch_size
+    start_time = time.time()
 
-    print(f"[Batch {batch_num}/{total_batches}] Processing...")
-    sub_data = data_image.slice(i, batch_size)
-     # extrait les ids
-    ids_df = sub_data.select(LIST_ID_COLUMNS)
-    image_path = sub_data["image_path"].to_list()
+    for i in range(0, data_image.height, batch_size):
+        
+        batch_num = i // batch_size
+        output_file = os.path.join(OUTPUT_DIR, f"features_image_chunk_{batch_num:04d}.parquet")
 
-    all_embeddings = extract_images_features(input_dir=IMAGE_FOLDER,image_paths=image_path,model=model,preprocess=preprocess)
-    #Construction du DataFrame Polars 
-    columns = [f"image_feat_{i}" for i in range(len(all_embeddings[0]))]
-    features_pl = pl.DataFrame(all_embeddings, schema=columns)
+        if os.path.exists(output_file):
+            continue
 
-    final_sub_df = pl.concat([ids_df, features_pl], how="horizontal")
+        print(f"[Batch {batch_num}/{total_batches}] Processing...")
+        sub_data = data_image.slice(i, batch_size)
+        # extrait les ids
+        ids_df = sub_data.select(LIST_ID_COLUMNS)
+        image_path = sub_data["image_path"].to_list()
 
-    if not os.path.exists(OUTPUT_DIR):
-        print(f"Dossier {OUTPUT_DIR} supprimé pendant l'exécution ! Je le recrée.")
-        os.makedirs(OUTPUT_DIR, exist_ok=True)
+        all_embeddings = extract_images_features(input_dir=IMAGE_FOLDER,image_paths=image_path,model=model,preprocess=preprocess)
+        #Construction du DataFrame Polars 
+        columns = [f"image_feat_{i}" for i in range(len(all_embeddings[0]))]
+        features_pl = pl.DataFrame(all_embeddings, schema=columns)
 
-    final_sub_df.write_parquet(output_file)
+        final_sub_df = pl.concat([ids_df, features_pl], how="horizontal")
 
-    del sub_data, ids_df, features_pl, final_sub_df, all_embeddings
-    gc.collect()
+        if not os.path.exists(OUTPUT_DIR):
+            print(f"Dossier {OUTPUT_DIR} supprimé pendant l'exécution ! Je le recrée.")
+            os.makedirs(OUTPUT_DIR, exist_ok=True)
 
-    log_progress(batch_num + 1, total_batches, start_time)
+        final_sub_df.write_parquet(output_file)
 
+        del sub_data, ids_df, features_pl, final_sub_df, all_embeddings
+        gc.collect()
+
+        log_progress(batch_num + 1, total_batches, start_time)
+
+    return f"images features exctract in {OUTPUT_DIR}"
+
+if __name__ == "__main__":
+    build_images_features_func(INPUT_CSV, LIST_ID_COLUMNS, IMAGE_FOLDER, OUTPUT_DIR)
--- src/features/build_text_features.py
+++ src/features/build_text_features.py
@@ -4,59 +4,61 @@ import gc
 from utils import extract_text_features_in_batches, clean_description, log_progress
 import time
 import os
-from dvclive import Live
-import subprocess
+
 # Paramètres
 INPUT_CSV = "/home/ubuntu/mar25_cmlops_rakuten/data/raw_data/X_train.csv"
 LIST_TEXT_COLUMN = ["designation","description"]
 LIST_ID_COLUMNS = ["imageid","productid"]
 OUTPUT_DIR = "/home/ubuntu/mar25_cmlops_rakuten/data/preprocessed/chunked_text_files"
 
-# Chargement des données et nettoyages
-data = pl.read_csv(INPUT_CSV)
-data = data.with_columns(pl.concat_str([pl.col(LIST_TEXT_COLUMN[0]),pl.col(LIST_TEXT_COLUMN[1])],separator=". ",ignore_nulls=True).alias('_'.join(LIST_TEXT_COLUMN)))
-data = data.with_columns(pl.col('_'.join(LIST_TEXT_COLUMN)).map_elements(lambda text:clean_description(text), return_dtype=pl.Utf8)).drop(LIST_TEXT_COLUMN)
-data = data.sort(LIST_ID_COLUMNS)
 
-tokenizer = DistilBertTokenizerFast.from_pretrained("distilbert-base-uncased")
-model = DistilBertModel.from_pretrained("distilbert-base-uncased")
+def build_text_features_func(INPUT_CSV,LIST_ID_COLUMNS,LIST_TEXT_COLUMN,OUTPUT_DIR):
+    # Chargement des données et nettoyages
+    data = pl.read_csv(INPUT_CSV)
+    data = data.with_columns(pl.concat_str([pl.col(LIST_TEXT_COLUMN[0]),pl.col(LIST_TEXT_COLUMN[1])],separator=". ",ignore_nulls=True).alias('_'.join(LIST_TEXT_COLUMN)))
+    data = data.with_columns(pl.col('_'.join(LIST_TEXT_COLUMN)).map_elements(lambda text:clean_description(text), return_dtype=pl.Utf8)).drop(LIST_TEXT_COLUMN)
+    data = data.sort(LIST_ID_COLUMNS)
 
-batch_size = 10000
-###
+    tokenizer = DistilBertTokenizerFast.from_pretrained("distilbert-base-uncased")
+    model = DistilBertModel.from_pretrained("distilbert-base-uncased")
 
-total_batches = (data.height + batch_size - 1) // batch_size
-start_time = time.time()
+    batch_size = 10000
+    ###
 
-for i in range(0, data.height, batch_size):
-    batch_start = time.time()
-    batch_num = i // batch_size
-    output_file = os.path.join(OUTPUT_DIR, f"features_text_chunk_{batch_num:04d}.parquet")
-    
-    if os.path.exists(output_file):
-        continue
-    print(f"[Batch {batch_num}/{total_batches}] Processing...")
-    sub_data = data.slice(i, batch_size)
-    # extrait les ids
-    ids_df = sub_data.select(LIST_ID_COLUMNS)
-    # extrait les textes
-    texts = sub_data.select('_'.join(LIST_TEXT_COLUMN)).to_series().to_list()
+    total_batches = (data.height + batch_size - 1) // batch_size
+    start_time = time.time()
 
-    # extraction des embeddings pour les textes
-    embeddings = extract_text_features_in_batches(texts,tokenizer=tokenizer,model=model)
+    for i in range(0, data.height, batch_size):
+        batch_start = time.time()
+        batch_num = i // batch_size
+        output_file = os.path.join(OUTPUT_DIR, f"features_text_chunk_{batch_num:04d}.parquet")
+        
+        if os.path.exists(output_file):
+            continue
+        print(f"[Batch {batch_num}/{total_batches}] Processing...")
+        sub_data = data.slice(i, batch_size)
+        # extrait les ids
+        ids_df = sub_data.select(LIST_ID_COLUMNS)
+        # extrait les textes
+        texts = sub_data.select('_'.join(LIST_TEXT_COLUMN)).to_series().to_list()
 
-    # Converti les embeddings en DataFrame Polars
-    columns = [f"text_feat_{i}" for i in range(embeddings.shape[1])]
-    features_pl = pl.DataFrame(embeddings, schema=columns)
+        # extraction des embeddings pour les textes
+        embeddings = extract_text_features_in_batches(texts,tokenizer=tokenizer,model=model)
 
-    # Ajoute les colonnes productid et imageid 
-    final_sub_df = pl.concat([ids_df, features_pl],how="horizontal")
-    
-    if not os.path.exists(OUTPUT_DIR):
-        print(f"Dossier {OUTPUT_DIR} supprimé pendant l'exécution ! Je le recrée.")
-        os.makedirs(OUTPUT_DIR, exist_ok=True)
-    
-    final_sub_df.write_parquet(output_file)
-    del sub_data, texts, embeddings, features_pl, ids_df, final_sub_df 
-    gc.collect()
-    log_progress(batch_num + 1, total_batches, start_time)
+        # Converti les embeddings en DataFrame Polars
+        columns = [f"text_feat_{i}" for i in range(embeddings.shape[1])]
+        features_pl = pl.DataFrame(embeddings, schema=columns)
+
+        # Ajoute les colonnes productid et imageid 
+        final_sub_df = pl.concat([ids_df, features_pl],how="horizontal")
+        
+        if not os.path.exists(OUTPUT_DIR):
+            print(f"Dossier {OUTPUT_DIR} supprimé pendant l'exécution ! Je le recrée.")
+            os.makedirs(OUTPUT_DIR, exist_ok=True)
+        
+        final_sub_df.write_parquet(output_file)
+        del sub_data, texts, embeddings, features_pl, ids_df, final_sub_df 
+        gc.collect()
+        log_progress(batch_num + 1, total_batches, start_time)
+    return f"images features exctract in {OUTPUT_DIR}"
    
